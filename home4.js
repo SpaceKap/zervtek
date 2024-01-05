1ade0b7ab01dd5c84e351b4c603d22bb14fd89b0
SpaceKap <148664117+SpaceKap@users.noreply.github.com>
2024-01-05 19:15:56 +0900

    Update home4.js

--- home4.js
+++ home4.js
@@ -188,41 +188,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 
-  $(function() {
-
-    let slideTotal = $("[data-swiper=lectures] .swiper-slide").length;
-    var swiper = new Swiper('[data-swiper=lectures]', {
-      speed: 800,
-      loop: true,
-      loopedSlides: slideTotal,
-      navigation: {
-        nextEl: '[data-swiper=next-lectures]',
-        prevEl: '[data-swiper=prev-lectures]',
-      },
-      breakpoints: {
-        992: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-        },
-        320: {
-          slidesPerView: 'auto',
-          spaceBetween: 24,
-        },
-      }
-    });
-
-    function animSlide() {
-      $(`.swiper-slide:not(:eq(${swiper.activeIndex}))`).find('[data-lectures=anim]').removeClass('active');
-      $(`.swiper-slide:eq(${swiper.activeIndex})`).find('[data-lectures=anim]').addClass('active');
-    }
-    swiper.on('transitionStart', function() {
-      setTimeout(animSlide, 800);
-      $(`.swiper-slide:not(:eq(${swiper.activeIndex}))`).find('[data-lectures=anim2]').removeClass('active');
-      $(`.swiper-slide:eq(${swiper.activeIndex})`).find('[data-lectures=anim2]').addClass('active');
-    });
-
-  });     
-
 
 //Contact form 
 // Script to combine phone parts into 'Full phone'
