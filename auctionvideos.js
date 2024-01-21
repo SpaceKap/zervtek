77082e17a2094b3398eee471109446b071a30089
SpaceKap <148664117+SpaceKap@users.noreply.github.com>
2024-01-21 20:36:17 +0900

    Create auctionvideos.js

---
+++ auctionvideos.js
@@ -0,0 +1,41 @@
+<video id="myVideo" playsinline autoplay muted loop width="100%" style="border-radius: 25px; overflow: hidden; border: none; box-shadow: none;">
+    <source src="https://s3.amazonaws.com/webflow-prod-assets/6569ab4a66eaeb8146075fb8/65a53a0d79dbe79817992ec5_japan%20map%20auction%20mp4_1.mp4" type="video/mp4">
+</video>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var container = document.querySelector('[data-video-container="uniqueValue"]');
+        var video = document.getElementById('myVideo');
+        if(container && video) {
+            container.appendChild(video);
+        }
+    });
+</script>
+
+<video id="myVideo2" playsinline autoplay muted loop width="100%" style="border-radius: 25px; overflow: hidden; border: none; box-shadow: none;">
+    <source src="https://s3.amazonaws.com/webflow-prod-assets/6569ab4a66eaeb8146075fb8/65a53ca280839cf425c5fa1b_auction%20landscap.mp4" type="video/mp4">
+</video>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var container = document.querySelector('[data-video-container1="uniqueValue1"]');
+        var video = document.getElementById('myVideo2');
+        if(container && video) {
+            container.appendChild(video);
+        }
+    });
+</script>
+
+<video id="myVideo3" playsinline autoplay muted loop width="100%" style="border-radius: 25px; overflow: hidden; border: none; box-shadow: none;">
+    <source src="https://s3.amazonaws.com/webflow-prod-assets/6569ab4a66eaeb8146075fb8/65a9d84e41b01384475b8c8b_sales%20data%20for%20JDMs%201.mp4" type="video/mp4">
+</video>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var container = document.querySelector('[data-video-container3="uniqueValue3"]');
+        var video = document.getElementById('myVideo3');
+        if(container && video) {
+            container.appendChild(video);
+        }
+    });
+</script>
